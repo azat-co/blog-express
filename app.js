ea64ba0617bb099b5c3a1815f789551369a587ee
Azat Mardanov <azat.mardanov@gmail.com>
2013-11-29 15:08:49 -0800

    add bootstrap, format html, version for chapters 2-3 :goberserk:

--- app.js
+++ app.js
@@ -19,12 +19,15 @@ var collections = {
 
 
 var app = express();
-app.locals.appTitle = "Express.js Blog App"
+app.locals.appTitle = "blog-express";
+
 app.use(function(req, res, next) {
   if (!collections.articles || ! collections.users) return next(new Error("No collections."))
   req.collections = collections;
   return next();
 });
+
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -37,8 +40,14 @@ app.use(express.cookieParser('3CCC4ACD-6ED1-4844-9217-82131BDCB239'));
 app.use(express.cookieSession({secret: '2C44774A-D649-4D44-9535-46E296EF984F'}))
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  if (req.session && req.session.admin)
+    res.locals.admin = true;
+  next();
+})
 
 // development only
 if ('development' == app.get('env')) {
@@ -46,7 +55,7 @@ if ('development' == app.get('env')) {
 }
 
 
-
+app.use(app.router);
 //PAGES&ROUTES
 app.get('/', routes.index);
 app.get('/login', routes.user.login);
@@ -60,6 +69,7 @@ app.get('/articles', routes.article.list)
 app.post('/articles', routes.article.add);
 app.put('/articles/:id', routes.article.edit);
 app.del('/articles/:id', routes.article.del);
+
 
 
 app.all('*',function(req, res) {
