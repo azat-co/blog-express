1c2cd05b86b0a7d6baefecfaa8c77c55e3d768a0
Azat Mardanov <azat.mardanov@gmail.com>
2013-12-25 13:12:45 -0800

    add travis config and mocha test for homepage (should test server + mongodb) :musical_score:

--- app.js
+++ app.js
@@ -87,6 +87,25 @@ app.all('*', function(req, res) {
   res.send(404);
 })
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function(){
+  // console.log('Express server listening on port ' + app.get('port'));
+// });
+
+var server = http.createServer(app);
+var boot = function () {
+  server.listen(app.get('port'), function(){
+    console.info('Express server listening on port ' + app.get('port'));
+  });
+}
+var shutdown = function() {
+  server.close();
+}
+if (require.main === module) {
+  boot();
+}
+else {
+  console.info('Running app as a module')
+  exports.boot = boot;
+  exports.shutdown = shutdown;
+  exports.port = app.get('port');
+}
