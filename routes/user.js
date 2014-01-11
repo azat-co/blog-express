d7c5a8a0b52cd8868ee53091ea05baec5494ec90
Azat Mardanov <azat.mardanov@gmail.com>
2014-01-11 10:42:57 -0800

    format code

--- routes/user.js
+++ routes/user.js
@@ -33,7 +33,10 @@ exports.logout = function(req, res, next) {
 exports.authenticate = function(req, res, next) {
   if (!req.body.email || !req.body.password)
     return res.render('login', {error: "Please enter your email and password."});
-  req.collections.users.findOne({email: req.body.email, password: req.body.password}, function(error, user){
+  req.collections.users.findOne({
+    email: req.body.email,
+    password: req.body.password
+  }, function(error, user){
     if (error) return next(error);
     if (!user) return res.render('login', {error: "Incorrect email&password combination."});
     req.session.user = user;
